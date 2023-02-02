1cde8b29c854d414b31e582dfad26cd242ed340e
xuerzong <xuerzong@gmail.com>
2023-02-03 00:04:49 +0800

    Fix: Synchronize styles of tag and account

--- src/components/options/Accounts.tsx
+++ src/components/options/Accounts.tsx
@@ -1,3 +1,6 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import cls from 'classnames'
 import { setCurrentAccount, setDeleteAccountModalVisible } from '@/stores/app'
 import useAccounts from '@/stores/accounts'
 import { setKey as setAccountKey } from '@/stores/account-key'
@@ -5,6 +8,7 @@ import { setOpen as setAccountModalOpen } from '@/stores/account-modal'
 import Empty from '../common/Empty'
 import IconButton from '../common/IconButton'
 import Icon from '../common/Icon'
+import { DotsHorizontalIcon } from '../common/DotsHorizontal'
 import { UserPlusIcon } from '../icons/UserPlus'
 import { DeleteIcon } from '../icons/Delete'
 import { EditIcon } from '../icons/Edit'
@@ -12,12 +16,12 @@ import { EditIcon } from '../icons/Edit'
 const Accounts: React.FC = () => {
   const accounts = useAccounts((state) => state.accounts)
 
-  const handleEditAccount = (account?: string) => {
+  const onEdit = (account?: string) => {
     setAccountKey(account || '')
     setAccountModalOpen(true)
   }
 
-  const handleDelete = async (account: string) => {
+  const onDelete = async (account: string) => {
     setCurrentAccount(account)
     setDeleteAccountModalVisible(true)
   }
@@ -28,7 +32,7 @@ const Accounts: React.FC = () => {
         <span className="font-bold uppercase">Account List</span>
         <div className="flex-1" />
 
-        <IconButton onClick={() => handleEditAccount()}>
+        <IconButton onClick={() => onEdit()}>
           <Icon size="sm" as={<UserPlusIcon />} />
         </IconButton>
       </div>
@@ -38,23 +42,74 @@ const Accounts: React.FC = () => {
           accounts.map((account) => (
             <div
               key={account.key}
-              className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer"
+              className="flex items-center px-2 py-3 hover:bg-gray-100 rounded transition-all"
             >
               {account.key}
 
               <div className="flex-1" />
 
-              <div className="space-x-2">
-                <IconButton onClick={() => handleEditAccount(account.key)}>
-                  <Icon as={<EditIcon />} size="sm" />
-                </IconButton>
-                <IconButton
-                  className="text-red-500"
-                  onClick={() => handleDelete(account.key)}
+              <Menu as="div" className="relative inline-block">
+                <div>
+                  <Menu.Button>
+                    <Icon
+                      size="md"
+                      className="text-gray-500 cursor-pointer"
+                      as={<DotsHorizontalIcon />}
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
                 >
-                  <Icon as={<DeleteIcon />} size="sm" />
-                </IconButton>
-              </div>
+                  <Menu.Items className="absolute right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded bg-white shadow z-10">
+                    <div className="p-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={cls(
+                              'w-full flex items-center transition-colors p-1 rounded',
+                              { 'bg-primary-600 text-white': active }
+                            )}
+                            onClick={onEdit.bind(null, account.key)}
+                          >
+                            <Icon
+                              className="mr-1"
+                              size="sm"
+                              as={<EditIcon />}
+                            />
+                            Edit
+                          </button>
+                        )}
+                      </Menu.Item>
+
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={cls(
+                              'w-full flex items-center transition-colors p-1 rounded',
+                              { 'bg-primary-600 text-white': active }
+                            )}
+                            onClick={onDelete.bind(null, account.key)}
+                          >
+                            <Icon
+                              className="mr-1"
+                              size="sm"
+                              as={<DeleteIcon />}
+                            />
+                            Delete
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </div>
           ))
         ) : (
